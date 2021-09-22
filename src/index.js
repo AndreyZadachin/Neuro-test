fe0b038de80f19b62ff76a851e94e8a99fa1eeb3
Andrey Zadachin <zadachin86@gmail.com>
2021-09-23 00:41:51 +0300

    Well done

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./pages/index.css";
+/*import FormValidate from "./script/components/FormValidate";
+import NewsApi from "./script/modules/NewsApi";
+import NewsCard from "./script/components/NewsCard";
+import NewsCardList from "./script/components/NewsCardList";
+import DataStorage from "./script/modules/DataStorage";
+*/
