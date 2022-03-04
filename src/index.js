10aaf7ffa6c7dfc4e9d50d3e4589ff7a0c6be57d
Andrey Zadachin <zadachin86@gmail.com>
2022-03-04 15:51:19 +0300

    some fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-import "./pages/index.css";
-/*import FormValidate from "./script/components/FormValidate";
-import NewsApi from "./script/modules/NewsApi";
-import NewsCard from "./script/components/NewsCard";
-import NewsCardList from "./script/components/NewsCardList";
-import DataStorage from "./script/modules/DataStorage";
-*/
+import './pages/index.css';
