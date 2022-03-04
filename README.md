bce7c0d041a92c1e32618e6e81aa26e207aee36e
Andrey Zadachin <zadachin86@gmail.com>
2022-03-04 16:25:06 +0300

    first commit

--- README.md
+++ README.md
@@ -1,21 +1,7 @@
-## WebpackConfig
+## Neuro-test
 
 ======
 
-### Webpack config stable
+## Level-2
 
-#### version v1.0.0
-
-Инструкция по развертыванию:
-
-1. Склонировать в локальный репозиторий - git clone https://github.com/AndreyZadachin/WebpackConfig.git
-2. Установить пакеты (должен быть установлен node.js <https://nodejs.org/en/>) - npm i
-3. Сборка пакета - npm run build
-4. Запуск на локальном сервере - npm run dev
-
-src - script:
-
-1. components - CommitCard, CommitCardList, FormValidate, NewsCard, NewsCardList
-2. constans - constans
-3. modules - DataStorage, GithubApi, NewsApi
-4. utils - formattingDate
+2. Нарисуйте на canvas основные геометрические фигуры(окружность, треугольник, квадрат, прямоугольник), расположите на холсте размером 800\*600 по 4-м углам.(добавьте js анимацию фигурам - опционально/будет плюсом)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,77 @@
-import "./pages/index.css";
-/*import FormValidate from "./script/components/FormValidate";
-import NewsApi from "./script/modules/NewsApi";
-import NewsCard from "./script/components/NewsCard";
-import NewsCardList from "./script/components/NewsCardList";
-import DataStorage from "./script/modules/DataStorage";
-*/
+import './pages/index.css';
+
+class Draw {
+  constructor(options) {
+    this.name = options.name;
+    this.canvas = document.getElementById(options.selector);
+    this.size = options.size;
+    this.ctx = this.canvas.getContext('2d');
+  }
+  drawRect() {
+    this.ctx.beginPath();
+    this.ctx.rect.apply(this.ctx, this.size);
+    this.ctx.closePath();
+    this.ctx.stroke();
+    this.ctx.fill();
+  }
+  drawArc() {
+    this.ctx.beginPath();
+    this.ctx.arc.apply(this.ctx, this.size.concat([2 * Math.PI, false]));
+    this.ctx.closePath();
+    this.ctx.stroke();
+    this.ctx.fill();
+  }
+  drawTriangle() {
+    this.ctx.beginPath();
+    this.ctx.moveTo.apply(this.ctx, this.size[0]);
+    this.ctx.lineTo.apply(this.ctx, this.size[1]);
+    this.ctx.lineTo.apply(this.ctx, this.size[2]);
+    this.ctx.lineCap = 'butt';
+    this.ctx.closePath();
+    this.ctx.stroke();
+    this.ctx.fill();
+  }
+  drawFigure() {
+    switch (this.name) {
+      case 'arc':
+        this.drawArc();
+        break;
+      case 'rect':
+        this.drawRect();
+        break;
+      case 'triangle':
+        this.drawTriangle();
+        break;
+    }
+  }
+}
+
+const drawRect = new Draw({
+  selector: 'canvas',
+  name: 'rect',
+  size: [450, 350, 300, 200],
+});
+const drawSquare = new Draw({
+  selector: 'canvas',
+  name: 'rect',
+  size: [100, 350, 200, 200],
+});
+const drawArc = new Draw({
+  selector: 'canvas',
+  name: 'arc',
+  size: [200, 150, 100],
+});
+const drawTriangle = new Draw({
+  selector: 'canvas',
+  name: 'triangle',
+  size: [
+    [500, 250],
+    [600, 50],
+    [700, 250],
+  ],
+});
+
+drawArc.drawFigure();
+drawTriangle.drawFigure();
+drawRect.drawFigure();
+drawSquare.drawFigure();
