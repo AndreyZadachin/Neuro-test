d508c08274d7ea77b00f715ee2657ffbf0f3820f
Andrey Zadachin <zadachin86@gmail.com>
2022-03-04 12:15:52 +0300

    first commit

--- README.md
+++ README.md
@@ -1,21 +1,7 @@
-## WebpackConfig
+## Neuro-test
 
 ======
 
-### Webpack config stable
+## Level-4
 
-#### version v1.0.0
-
-Инструкция по развертыванию:
-
-1. Склонировать в локальный репозиторий - git clone https://github.com/AndreyZadachin/WebpackConfig.git
-2. Установить пакеты (должен быть установлен node.js <https://nodejs.org/en/>) - npm i
-3. Сборка пакета - npm run build
-4. Запуск на локальном сервере - npm run dev
-
-src - script:
-
-1. components - CommitCard, CommitCardList, FormValidate, NewsCard, NewsCardList
-2. constans - constans
-3. modules - DataStorage, GithubApi, NewsApi
-4. utils - formattingDate
+7. Реализуйте получение JSON данных с помощью HTTP запроса (например https://reqres.in/api/users), проверку данных на корректность, вывод данных консоль/DOM
--- src/index.js
+++ src/index.js
@@ -1,7 +1,84 @@
-import "./pages/index.css";
-/*import FormValidate from "./script/components/FormValidate";
-import NewsApi from "./script/modules/NewsApi";
-import NewsCard from "./script/components/NewsCard";
-import NewsCardList from "./script/components/NewsCardList";
-import DataStorage from "./script/modules/DataStorage";
-*/
+import './pages/index.css';
+
+class UsersCard {
+  constructor() {}
+
+  createCardUsers(cardUsers) {
+    const usersContainer = document.createElement('div');
+    usersContainer.classList.add('users__container');
+
+    const usersImage = document.createElement('img');
+    usersImage.classList.add('users__image');
+
+    const usersEmail = document.createElement('p');
+    usersEmail.classList.add('users__email');
+
+    const usersName = document.createElement('p');
+    usersName.classList.add('users__name');
+
+    const usersLastName = document.createElement('p');
+    usersLastName.classList.add('users__last-name');
+
+    usersContainer.appendChild(usersImage);
+    usersContainer.appendChild(usersEmail);
+    usersContainer.appendChild(usersName);
+    usersContainer.appendChild(usersLastName);
+
+    usersImage.setAttribute('src', cardUsers.avatar);
+    usersImage.setAttribute('alt', 'Здесь должен быть аватар пользователя');
+    usersEmail.textContent = cardUsers.email;
+    usersName.textContent = cardUsers.first_name;
+    usersLastName.textContent = cardUsers.last_name;
+
+    return usersContainer;
+  }
+}
+
+class UsersCardList {
+  constructor(card) {
+    this.card = card;
+  }
+
+  renderUsersCard(carouselCell) {
+    this.card.append(carouselCell);
+  }
+}
+
+class UsersApi {
+  constructor(urlUsers) {
+    this.url = urlUsers;
+  }
+
+  getUsers() {
+    return fetch(`${this.url}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => this.responce(res))
+      .catch((err) => {
+        throw err;
+      });
+  }
+
+  responce(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Что-то пошло не так: ${res.status}`);
+  }
+}
+
+const elemContainerForUsers = document.querySelector('.section');
+const baseUrl = 'https://reqres.in/api/users';
+const usersCard = new UsersCard();
+const usersCardList = new UsersCardList(elemContainerForUsers);
+const usersApi = new UsersApi(baseUrl);
+
+usersApi.getUsers().then((res) => {
+  res.data.forEach((newsContainer) => {
+    usersCardList.renderUsersCard(usersCard.createCardUsers(newsContainer));
+  });
+  console.log(res.data);
+});
